92f7c6a7fecb08f63db731088f0f79966a6127cd
HELIO POTELICKI <helio.p2000@hotmail.com>
2020-09-18 16:54:14 -0300

    main

--- Trabalho01/Analisa_Matriz.java
+++ Trabalho01/Analisa_Matriz.java
@@ -8,5 +8,8 @@ public class Analisa_Matriz {
         int[][] matrizAdj = { { 0, 1, 0 }, { 1, 0, 1 }, { 0, 1, 0 } };
         Grafo grafo = new Grafo(matrizAdj);
         grafo.printMatrizAdj();
+        System.out.printf("Este grafo é do tipo: %s \n", grafo.tipoDoGrafo());
+        System.out.printf("Que possui: %s \n", grafo.arestasDoGrafo());
+        System.out.printf(grafo.grausDoVertice());
     }
 }
