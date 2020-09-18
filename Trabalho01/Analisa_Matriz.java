6b10447b213fbcf4c5d53251cb7210bbad9ef636
HELIO POTELICKI <helio.p2000@hotmail.com>
2020-09-18 13:22:28 -0300

    create files

---
+++ Trabalho01/Analisa_Matriz.java
@@ -0,0 +1,10 @@
+public class Analisa_Matriz {
+    public static void main(String[] args) {
+        Analisa_Matriz analize = new Analisa_Matriz();
+        System.out.println(analize.analizar());
+    }
+
+    private String analizar() {
+        return "crack";
+    }
+}
