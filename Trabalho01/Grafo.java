2523878d18110ccabd91e51151a6244914363b22
HELIO POTELICKI <helio.p2000@hotmail.com>
2020-09-21 20:50:33 -0300

    regular, fix dirigido

--- Trabalho01/Grafo.java
+++ Trabalho01/Grafo.java
@@ -23,30 +23,46 @@ public class Grafo {
         String retorno = "";
         boolean diag = false;
         boolean maior = false;
+        boolean regular = false;
         int contDiag = 0;
         int contMaior = 0;
         int contDirig = 0;
+        int contNulo = 0;
+        int[] somaLinhas = new int[matrizAdj.length];
 
         for (int l = 0; l < matrizAdj.length; l++) {
+            int aux = 0;
             for (int c = 0; c < matrizAdj[0].length; c++) {
                 if (matrizAdj[l][c] < 2) {
                     contMaior++;
                 }
+                if (matrizAdj[l][c] == 0) {
+                    contNulo++;
+                }
                 if (matrizAdj[l][c] == matrizAdj[c][l]) {
                     contDirig++;
                 }
+                aux += matrizAdj[l][c];
             }
+            somaLinhas[l] = aux;
             if (matrizAdj[l][l] == 0) {
                 contDiag++;
             }
+            if (l >= 1 && l != somaLinhas.length) {
+                if (somaLinhas[l] == somaLinhas[l - 1]) {
+                    regular = true;
+                } else {
+                    regular = false;
+                }
+            }
         }
 
-        contDirig -= contDiag;
+        contDirig -= matrizAdj.length;
 
         if (contDirig == (tamanhoMatriz() - matrizAdj.length)) {
-            retorno += "Grafo dirigido, ";
-        } else {
             retorno += "Grafo não dirigido, ";
+        } else {
+            retorno += "Grafo dirigido, ";
         }
         if (contDiag == matrizAdj.length) {
             diag = true;
@@ -54,11 +70,21 @@ public class Grafo {
         if (contMaior == tamanhoMatriz()) {
             maior = true;
         }
-
         if (diag && maior) {
-            retorno += "Simples";
+            retorno += "Simples, ";
         } else {
-            retorno += "Multigrafo";
+            retorno += "Multigrafo, ";
+        }
+        if (regular) {
+            retorno += "regular, ";
+        } else {
+            retorno += "não regular, ";
+        }
+        if (contNulo == tamanhoMatriz()) {
+            retorno += "nulo";
+        }
+        if (contNulo == 0) {
+            retorno += "completo";
         }
         return retorno;
     }
--- Trabalho01/testes.java
+++
@@ -1,7 +0,0 @@
-public class testes {
-    public static void main(String[] args) {
-        int[][] matrizAdj = { { 0, 1, 0 }, { 1, 0, 1 }, { 0, 1, 0 } };
-        Grafo grafo = new Grafo(matrizAdj);
-        grafo.printMatrizAdj();
-    }
-}
